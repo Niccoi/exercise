814712fa0669cf07b7c63cdf70689d10134ab247
niccoi <niccoi1994@gmail.com>
2021-03-27 07:09:29 +0800

    added and modified function for todo list

--- todo.js
+++ todo.js
@@ -1,4 +1,193 @@
+todoItems = {todoList: []}
+counter = 0
+/*function baseTodo(name) {
+  return {
+    name: name,
+    items: []
+  }
+}
+// Array
+// Object
+// Manipulate object//array
+var models = {
+  todos: [baseTodo('Todo')]
+} */
 
+ todoview = {
+
+	clearList: function() {
+		var range = document.createRange();
+		range.selectNodeContents(document.getElementById("list"));
+		range.deleteContents();
+	},
+
+	render: function() {
+		
+		this.clearList();	
+		
+		if (todoItems.todoList.length != 0) {
+
+			list = document.getElementById("list")
+
+			for (var i = todoItems.todoList.length - 1; i >= 0; i--) {
+
+				item = document.createElement('li');
+				span = document.createElement('span');
+                checkBoxLabel = document.createElement('label');
+				checkBox = document.createElement('input');
+                checkBoxIcon = document.createElement('i')
+
+				dltButton = document.createElement('button');
+				dltIcon = document.createElement('i');
+				
+				item.className = "item"
+				span.className = "item-text"
+				checkBox.className = "item-complete"
+				dltButton.className = "item-delete"
+
+                // Add our icons
+				checkBoxLabel.setAttribute("class", "item__checkbox item__checkbox--3")
+				checkBox.setAttribute("type", "checkbox")
+                checkBox.setAttribute("data-id", i)
+                checkBoxIcon.setAttribute("data-id", i);
+                checkBoxIcon.setAttribute("class", "fas fa-check");
+
+				dltIcon.setAttribute("class", "fas fa-trash-alt")
+				dltIcon.setAttribute("data-id", i)
+				span.setAttribute("data-id", i)
+               
+				
+				span.textContent = todoItems.todoList[i].text
+				
+				// Put a line through any items we've 'ticked' off
+				if (todoItems.todoList[i].completed) {
+					span.setAttribute("style", "text-decoration: line-through; color: #bbb")
+				}
+
+
+				
+
+				// Add our onclick functions for complete/delete actions
+				dltIcon.onclick = function(e){
+					var id = e.target.getAttribute("data-id")
+					controller.deleteItem(id)
+				} 
+
+				checkBox.onclick = function(e){
+					var id = e.target.getAttribute("data-id")
+					controller.completeItem(id)
+				}
+				span.onclick = function(e){
+					var id = e.target.getAttribute("data-id")
+					controller.editItem(id)
+				}
+				//span.setAttribute("onclick","controller.editItem('" + i + "')")
+
+				// Append all our elements and add to DOM
+                
+                
+                checkBox.appendChild(checkBoxIcon);
+                checkBoxLabel.appendChild(checkBox);
+                dltButton.appendChild(dltIcon);
+
+                item.appendChild(checkBoxLabel);
+              // item.appendChild(checkBoxIcon);
+                item.appendChild(span);
+                item.appendChild(dltButton);
+                list.appendChild(item);
+
+			
+                 
+        
+			}		
+		} 
+	},
+  checkItem: function(item){
+    alreadyListed = 0;
+    for (var i = todoItems.todoList.length - 1; i >= 0; i--) {
+      if(todoItems.todoList[i].text == item && !todoItems.todoList[i].completed) {
+         alreadyListed = 1;        
+      }
+    }
+    if(alreadyListed == 1){
+      alert("Value already listed")
+    document.getElementById("add-item").value = ""
+    alreadyListed = 0;
+    }
+    else{
+    listItem = { text: item, completed: false }
+    todoItems.todoList.push(listItem)
+    
+    document.getElementById("add-item").value = ""
+    todoview.render() 
+  }  
+  },
+
+	addItem: function(e) {
+		if ((e.code == "Enter") || (e.code == "NumpadEnter")) {
+      item = document.getElementById("add-item").value
+  
+			if (item != "") {     
+              controller.addItem(item);
+          return false;	
+            
+			}
+     
+
+      }
+	     
+	},
+
+
+}
+
+controller = {
+	init: function() {
+		todoview.render()
+	},
+	
+	addItem: function(item) {
+   
+    if(todoItems.todoList.length>0){
+      todoview.checkItem(item)
+    }
+    else{
+
+      listItem = { text: item, completed: false }
+      todoItems.todoList.push(listItem)
+      
+      document.getElementById("add-item").value = ""
+      todoview.render()   
+      
+     }
+	
+   
+	},
+	
+	completeItem: function(item_index) {
+		
+		todoItems.todoList[item_index].completed = !todoItems.todoList[item_index].completed
+		todoview.render()
+	},
+
+	deleteItem: function(item_index) {
+		todoItems.todoList.splice(item_index, 1)
+		todoview.render()
+	},
+	editItem: function(item_index){
+		if(todoItems.todoList[item_index].completed !== true){
+			var updatedTodo = prompt("Update todo");
+			todoItems.todoList[item_index].text = updatedTodo;
+			todoview.render();
+		}
+		
+	}
+}
+
+
+controller.init()
+
+/*
   var listArray =  new Array();
   var i = 1;
   var x = "";
@@ -102,7 +291,9 @@ input.addEventListener("keyup", function(event) {
         }
 
 
-       /*
+     */
+
+        /*
         for(var i=0; i<listArray.length; i++){
           
           listItems += `
@@ -116,5 +307,5 @@ input.addEventListener("keyup", function(event) {
           `
         }
 
-      document.getElementById("list").innerHTML = listItems;*/
-      }
+      document.getElementById("list").innerHTML = listItems;
+      } */
